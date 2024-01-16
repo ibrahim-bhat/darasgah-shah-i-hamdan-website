5d176538f595bf0ef776823a11ab98431cde2d2a
Ibrahim <ibrahimbhat053@gmail.com>
2024-01-16 09:14:05 +0530

    updated spinner on website

--- js/main.js
+++ js/main.js
@@ -2,14 +2,23 @@
     "use strict";
 
     // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
+var spinner = function () {
+  var timeout = 1000; // Default timeout for other pages
+
+  if (window.location.pathname.includes("index.html")) {
+    timeout = 2000; // Set 3 seconds timeout for index.html
+  }
+
+  setTimeout(function () {
+    if ($("#spinner").length > 0) {
+      $("#spinner").removeClass("show");
+    }
+  }, timeout);
+};
+    
+spinner();
+
+
     
     
     // Initiate the wowjs
@@ -55,17 +64,17 @@
     
     
     // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 300) {
+    //         $('.back-to-top').fadeIn('slow');
+    //     } else {
+    //         $('.back-to-top').fadeOut('slow');
+    //     }
+    // });
+    // $('.back-to-top').click(function () {
+    //     $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    //     return false;
+    // });
 
 
     // Header carousel
